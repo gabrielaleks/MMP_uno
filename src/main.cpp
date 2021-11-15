be8b1f3b0ade1e20803e298029ccc68b525daab9
Gabriel Aleks <gabriel.aleks@nano.ufrj.br>
2021-11-15 16:24:57 -0300

    Add state machine backbone

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,132 @@
 #include <Arduino.h>
 #include <Manager.h>
-#include <Ina219.h>
-#include <Bmp388.h>
-#include <Adafruit_INA219.h>
+#include <AdafruitSd.h>
 
 Manager manager;
-Bmp388 bmp;
 
-void setup(void) {
-    Serial.begin(9600);
-    
-	while (!Serial) {
-        delay(1);
-    }
-    // ina.begin();
-    // Serial.println(ina.getBusVoltage_V());
-    manager.init();
-    manager.systemTest();
-    // bmp.init();
-    // bmp.setSampling();
-    // if(!manager.checkI2cDevices()) {
-    //     Serial.println("deu ruim");
-    // } else {
-    //     Serial.println("deu bom");
+void handleStateMachine();
+
+void setup() {
+    // Cold start sequence
+    manager.startSerial();
+    manager.startStateMachine();
+    // manager.init();
+
+    // if (DEVELOPMENT) {
+    //     manager.systemTest();
     // }
+    
+    Log::println("\n--- STARTING ---");
+    
+    Manager::setState(StateMachine::PAD_IDLE);
 }
 
-void loop(void) {
-    // bmp.updateValues();
-    // bmp.printValues();
-    // delay(1000);
+void loop() {
+    handleStateMachine();
+}
+
+void handleStateMachine() {
+    switch (Manager::getState()) {
+        case (StateMachine::PAD_IDLE):
+            manager.startCountingTime();
+            manager.readSensors();
+
+            Serial.print("ta no estado: ");
+            Serial.println(Manager::getState());
+
+            delay(1000);
+
+            if (true) {
+                Manager::setState(StateMachine::POWERED_FLIGHT);
+            }
+
+            break;
+
+        case (StateMachine::POWERED_FLIGHT):
+            Serial.print("ta no estado: ");
+            Serial.println(Manager::getState());
+
+            delay(1000);
+
+            if (true) {
+                Manager::setState(StateMachine::APOGEE);
+            }
+
+            break;
+
+        case (StateMachine::APOGEE):
+            Serial.print("ta no estado: ");
+            Serial.println(Manager::getState());
+
+            delay(1000);
+
+            if (true) {
+                Manager::setState(StateMachine::DROGUE_DEPLOYMENT);
+            }
+
+            break;
+
+        case (StateMachine::DROGUE_DEPLOYMENT):
+            Serial.print("ta no estado: ");
+            Serial.println(Manager::getState());
+
+            delay(1000);
+
+            if (true) {
+                Manager::setState(StateMachine::MAIN_DEPLOYMENT);
+            }
+
+            break;
+
+        case (StateMachine::MAIN_DEPLOYMENT):
+            Serial.print("ta no estado: ");
+            Serial.println(Manager::getState());
+
+            delay(1000);
+
+            if (true) {
+                Manager::setState(StateMachine::LANDED);
+            }
+
+            break;
+
+        case (StateMachine::LANDED):
+            Serial.print("ta no estado: ");
+            Serial.println(Manager::getState());
+
+            delay(1000);
+
+            if (true) {
+                Manager::setState(StateMachine::ABORT);
+            }
+
+            break;
+
+        case (StateMachine::ABORT):
+            Serial.print("ta no estado: ");
+            Serial.println(Manager::getState());
+
+            delay(1000);
+
+            if (true) {
+                Manager::setState(StateMachine::HOT_START);
+            }
+
+            break;
+
+        case (StateMachine::HOT_START):
+            Serial.print("ta no estado: ");
+            Serial.println(Manager::getState());
+
+            delay(1000);
+
+            if (true) {
+                Manager::setState(StateMachine::PAD_IDLE);
+            }
+
+            break;
+        
+        default:
+            break;
+        }
 }
